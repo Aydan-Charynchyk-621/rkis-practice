68fb8aea2864baa48e66044246569aa64aa9bbd6
Aydan <ajdancaryncyk@gmail.com>
2022-11-12 01:19:17 +0700

    First commit. Create console app

---
+++ bin/rcis_ep_task_three.dart
@@ -0,0 +1,5 @@
+import 'package:rcis_ep_task_three/rcis_ep_task_three.dart' as rcis_ep_task_three;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rcis_ep_task_three.calculate()}!');
+}
---
+++ test/rcis_ep_task_three_test.dart
@@ -0,0 +1,8 @@
+import 'package:rcis_ep_task_three/rcis_ep_task_three.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
