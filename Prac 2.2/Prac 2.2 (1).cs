0a89c69bf1a9b3f23815b82efaeea46583f9a615
Charynchyk <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2023-03-17 11:41:40 +0700

    Update Prac 2.2 (1).cs

--- Prac 2.2/Prac 2.2 (1).cs
+++ Prac 2.2/Prac 2.2 (1).cs
@@ -9,23 +9,24 @@ public class Program
         private int group;
         private int[] progress = {};
 
-
         public void Info()
         {
             Console.WriteLine("Фамилия: {0}\nДата Рождения: {1}\nГруппа: {2}", surname, dateOfBirth, group);
-            foreach(int arr in progress)
-            Console.Write(arr + " ");
+            foreach(int grades in progress)
+            Console.Write(grades + " ");
         }
         
         public void Changeinfo(string surname)
         {
             this.surname = surname;
         }
+        
         public void Changeinfo(string surname, DateTime dateOfBirth)
         {
             this.surname = surname;
             this.dateOfBirth = dateOfBirth;
         }
+        
         public void Changeinfo(string surname, DateTime dateOfBirth, int group)
         {
             this.surname = surname;
@@ -44,9 +45,9 @@ public class Program
 
     static void Main()
     {
-        Student myInfo = new Student("Maksimov", new DateTime(1991, 01, 30), 714, new int [5] {4, 3, 4, 5, 2});
+        Student myInfo = new Student("Khertek", new DateTime(1991, 01, 30, 11, 30, 0), 714, new int [5] {4, 3, 4, 5, 2});
 
-        myInfo.Changeinfo(surname: "Charynchyk", dateOfBirth: new DateTime(2004, 11, 27), group: 621);
+        myInfo.Changeinfo(surname: "Charynchyk", dateOfBirth: new DateTime(2004, 11, 27, 3, 0, 0), group: 621);
         myInfo.Info();
     }
 }
