022484152c1d6e00278b6cfe60033c4b2ebff9aa
Charynchyk <117069700+Aydan-Charynchyk-621@users.noreply.github.com>
2023-03-17 12:10:08 +0700

    Update Prac 2.2 (5).cs

--- Prac 2.2/Prac 2.2 (5).cs
+++ Prac 2.2/Prac 2.2 (5).cs
@@ -53,12 +53,14 @@ class Program
     {
         designer object1 = new designer(621, "Aydan"); 
         Console.WriteLine("Свойство первого объекта: " + object1.Property1 + ", " + object1.Property2);
+        
         designer object2 = new designer();
         Console.WriteLine("Свойство второго объекта: " + object2.Property1 + ", " + object2.Property2); 
 
         object1.Property1 = 2004;
         object1.Property2 = "год";
         Console.WriteLine("Свойство первого объекта после обновления: " + object1.Property1 + ", " + object1.Property2);
+        
         GC.Collect();
     }
 }
